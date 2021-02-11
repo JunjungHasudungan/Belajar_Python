1c62595f64347579755bbd9bf0e404c93c6dd4dd
JunjungHasudungan <junjunghasudungan43@gmail.com>
2021-02-11 21:09:20 +0700

    first commit

---
+++ Python_dasar/Tipe Data/Main.py
@@ -0,0 +1,21 @@
+#tipe data integer
+a = 10
+# cetak
+print("Nilai a adalah: ",a)
+
+# tipe data karakter (string)
+string = "Hasudungan Sitorus"
+print("Nama saya adalah: ", string) 
+
+# tipe data boolean (True/Falsse)
+boolean = True
+print("nilai dari a: ", boolean)
+
+# tipe data float (nilai desimal)
+data_float = 3.4
+print("nilai data float: ", data_float)
+
+# tipe data khusus
+
+dataComplex = complex(3,4)
+print("nilai data complex", type(dataComplex))
