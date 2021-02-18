1363523610592e3534e2502ad8b17bf8f99919be
JunjungHasudungan <junjunghasudungan43@gmail.com>
2021-02-18 20:25:17 +0700

    update

---
+++ Python_dasar/fungsi/fungsi return/Main.py
@@ -0,0 +1,18 @@
+# create some function with return
+def perkalian(a, b):
+    hasil = a * b
+    return hasil
+
+perkalian = perkalian(2,3)
+print("Hasil perkalian = ", perkalian)
+print(" ")
+print("Contoh Fungsi ke-2 ")
+
+def diskon(hargaDasar):
+    hasilDiskon = hargaDasar - (hargaDasar * 10/ 100)
+    return hasilDiskon
+
+hargaAwal = 10000
+total = diskon(hargaAwal)
+print("Harga awal: ", hargaAwal)
+print("Harga Setelah diskon:", total)
---
+++ Python_dasar/while-loop/Main.py
@@ -0,0 +1,8 @@
+# create some data member
+i = 0
+
+while i < 10:
+    print(i)
+    if i == 5:
+        break
+    i + 1
